ace841a3c71c0dbae35580eaa8ea121a6804e5be
snjku <snjkumarbr@gmail.com>
2021-10-16 08:52:19 +0530

    c

---
+++ demo-code/src/main/java/com/example/demo/DemoCodeApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCodeApplication.class, args);
+	}
+//	
+//	@GetMapping("/hello")
+//	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//		return String.format("Hello %s!", name);
+//		}
+
+
+}
